4603e611701ffe33a8b03a6941e1bb91fe7d7780
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2025-07-07 23:30:53 +0700

    add method channel

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
-import 'package:core_flutter_flutterside/f_cupertino_text_field.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyHomePage());
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static const platform = MethodChannel('natasharadika.flutter.dev/note');
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _sendNoteToNative() async {
+    final note = _textController.text;
+    try {
+      // call method on ios
+      await platform.invokeMethod('saveNote', {"note": note});
+      print("note sent: $note");
+    } on PlatformException catch (e) {
+      print("failed to send note ${e.message}");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +45,31 @@ class MyHomePage extends StatelessWidget {
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                    const FCupertinoTextField(),
+                    SizedBox(
+                      height: 500,
+                      child: CupertinoTextField(
+                        controller: _textController,
+                        placeholder: "Enter note..",
+                        maxLines: null,
+                        expands: true,
+                        autocorrect: true,
+                        enableSuggestions: true,
+                        textCapitalization: TextCapitalization.sentences,
+                        keyboardType: TextInputType.multiline,
+                        textAlignVertical: TextAlignVertical.top,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: CupertinoColors.lightBackgroundGray,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(16.0)),
+                      ),
+                    ),
                     const SizedBox(height: 8.0),
                     CupertinoButton.filled(
                         color: CupertinoColors.activeBlue,
-                        onPressed: () {
-                          print("ok");
-                        },
+                        onPressed: _sendNoteToNative,
                         child: const Text("Save Note")),
                   ])),
             )));
